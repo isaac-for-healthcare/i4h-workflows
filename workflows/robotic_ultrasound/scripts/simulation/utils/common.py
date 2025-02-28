01b90b192a14290deedf6dcb1401b184ee092bdf
YunLiu <55491388+KumoLiu@users.noreply.github.com>
2025-02-28 16:53:11 +0800

    add visualization
    Signed-off-by: YunLiu <55491388+KumoLiu@users.noreply.github.com>

---
+++ workflows/robotic_ultrasound/scripts/simulation/utils/common.py
@@ -0,0 +1,42 @@
+import os
+from importlib.util import module_from_spec, spec_from_file_location
+
+import numpy as np
+
+from simulation.configs.config import Config
+
+
+def colorize_depth(depth_data: np.ndarray, near=1.0, far=50.0) -> np.ndarray:
+    # https://docs.omniverse.nvidia.com/extensions/latest/ext_replicator/
+    # programmatic_visualization.html#helper-visualization-functions
+
+    depth_data = np.clip(depth_data, near, far)
+    depth_data = (np.log(depth_data) - np.log(near)) / (np.log(far) - np.log(near))
+    depth_data = 1.0 - depth_data
+    depth_data_uint8 = (depth_data * 255).astype(np.uint8)
+
+    return depth_data_uint8
+
+
+def list_exp_configs(configs_path: str | None = None):
+    if configs_path is None:
+        configs_path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), "configs"))
+    names = [c[:-3] for c in os.listdir(configs_path) if c.endswith(".py")]
+    return [n for n in names if n not in ["__init__", "config"]]
+
+
+def get_exp_config(name: str, configs_path: str | None = None) -> Config:
+    if configs_path is None:
+        configs_path = os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), "configs"))
+    module_path = os.path.join(configs_path, f"{name}.py")
+
+    spec = spec_from_file_location(name, module_path)
+    if spec is None or spec.loader is None:
+        raise Exception(f"Could not load spec for {name} from {configs_path}")
+
+    module = module_from_spec(spec)
+    if module is None:
+        raise Exception(f"Could not load module for {name} from {configs_path}")
+
+    spec.loader.exec_module(module)
+    return module.config
---
+++ workflows/robotic_ultrasound/tests/test_simulation/common_config.py
@@ -0,0 +1,17 @@
+from simulation.configs.config import (
+    Config,
+    FrankaConfig,
+    RoomCameraConfig,
+    TargetConfig,
+    UltraSoundConfig,
+    WristCameraConfig,
+)
+
+config = Config(
+    main_usd_path="./basic.usda",
+    room_camera=RoomCameraConfig(prim_path="/RoomCamera", enabled=True),
+    wrist_camera=WristCameraConfig(prim_path="/Franka/panda_hand/geometry/realsense/realsense_camera", enabled=True),
+    franka=FrankaConfig(prim_path="/Franka", ik=False, auto_pos=False, enabled=True),
+    target=TargetConfig(prim_path="/Target", auto_pos=False, enabled=True),
+    ultrasound=UltraSoundConfig(prim_path="/Target", enabled=True),
+)
