8684365b5917aad93db80cf2e4443cae0ca00281
Nic Ma <nma@nvidia.com>
2025-03-03 17:12:52 +0800

    [MedTech] update to use Publisher and Subscriber
    Signed-off-by: Nic Ma <nma@nvidia.com>

--- workflows/robotic_ultrasound/scripts/policy_runner/run_policy.py
+++ workflows/robotic_ultrasound/scripts/policy_runner/run_policy.py
@@ -1,15 +1,14 @@
 import argparse
-import asyncio
 import os
 import numpy as np
-import rti.asyncio
-import rti.connextdds as dds
 from PIL import Image
 
 from rti_dds.schemas.camera_info import CameraInfo
 from rti_dds.schemas.franka_ctrl import FrankaCtrlInput
 from rti_dds.schemas.franka_info import FrankaInfo
 
+from rti_dds.publisher import Publisher
+from rti_dds.subscriber import SubscriberWithCallback
 from policy_runner.runners import PI0PolicyRunner
 
 current_state = {
@@ -17,7 +16,6 @@ current_state = {
     "wrist_cam": None,
     "joint_pos": None,
 }
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -56,69 +54,64 @@ def main():
     if args.rti_license_file is None or not os.path.isabs(args.rti_license_file):
         raise ValueError("RTI license file must be an existing absolute path.")
     os.environ["RTI_LICENSE_FILE"] = args.rti_license_file
-    dp = dds.DomainParticipant(domain_id=args.domain_id)
-    r_cam_reader = dds.DataReader(dp.implicit_subscriber, dds.Topic(dp, args.topic_in_room_camera, CameraInfo))
-    w_cam_reader = dds.DataReader(dp.implicit_subscriber, dds.Topic(dp, args.topic_in_wrist_camera, CameraInfo))
-    f_pos_reader = dds.DataReader(dp.implicit_subscriber, dds.Topic(dp, args.topic_in_franka_pos, FrankaInfo))
-    writer = dds.DataWriter(dp.implicit_publisher, dds.Topic(dp, args.topic_out, FrankaCtrlInput))
+    hz = 1 / 30
 
-    pi0_policy = PI0PolicyRunner(ckpt_path=args.ckpt_path, repo_id=args.repo_id)
+    class PolicyPublisher(Publisher):
+        def __init__(self, topic: str, domain_id: int):
+            super().__init__(topic, FrankaCtrlInput, hz, domain_id)
 
-    async def read_room():
-        print(f"{args.domain_id}:{args.topic_in_room_camera} => Waiting for data...")
-        async for data in r_cam_reader.take_data_async():
+        def produce(self, dt: float, sim_time: float):
+            r_cam_buffer = np.frombuffer(current_state["room_cam"], dtype=np.uint8)
+            room_cam = Image.fromarray(r_cam_buffer.reshape(args.height, args.width, 4), "RGBA").convert("RGB")
+            w_cam_buffer = np.frombuffer(current_state["wrist_cam"], dtype=np.uint8)
+            wrist_cam = Image.fromarray(w_cam_buffer.reshape(args.height, args.width, 4), "RGBA").convert("RGB")
+            joint_pos = current_state["joint_pos"]
+
+            actions = pi0_policy.infer(
+                room_cam=np.array(room_cam),
+                wrist_cam=np.array(wrist_cam),
+                joint_pos=np.array(joint_pos[:7]),
+            )
+            actions = np.array(actions).astype(np.float32)
+
+            # First action only
+            for action in actions:
+                target_jp = [i for i in joint_pos]
+                for i in range(len(action)):
+                    target_jp[i] = target_jp[i] + action[i]
+
+                i = FrankaCtrlInput()
+                i.joint_positions = target_jp
+                return i
+
+
+    writer = PolicyPublisher(args.topic_out, args.domain_id)
+
+    def dds_callback(topic, data):
+        if topic == args.topic_in_room_camera:
             o: CameraInfo = data
             current_state["room_cam"] = o.data
 
-    async def read_wrist():
-        print(f"{args.domain_id}:{args.topic_in_room_camera} => Waiting for data...")
-        async for data in w_cam_reader.take_data_async():
+        if topic == args.topic_in_wrist_camera:
             o: CameraInfo = data
             current_state["wrist_cam"] = o.data
 
-    async def read_pos():
-        print(f"{args.domain_id}:{args.topic_in_room_camera} => Waiting for data...")
-        async for data in f_pos_reader.take_data_async():
+        if topic == args.topic_in_franka_pos:
             o: FrankaInfo = data
             current_state["joint_pos"] = o.joints_state_positions
+            if current_state["room_cam"] and current_state["wrist_cam"] and current_state["joint_pos"]:
+                writer.write(0, 0)
 
-            mc = current_state["room_cam"]
-            wc = current_state["wrist_cam"]
-            jp = current_state["joint_pos"]
+    r_cam_reader = SubscriberWithCallback(dds_callback, args.domain_id, args.topic_in_room_camera, CameraInfo, hz)
+    w_cam_reader = SubscriberWithCallback(dds_callback, args.domain_id, args.topic_in_wrist_camera, CameraInfo, hz)
+    f_pos_reader = SubscriberWithCallback(dds_callback, args.domain_id, args.topic_in_franka_pos, FrankaInfo, hz)
 
-            if mc is not None and wc is not None and jp is not None:
-                r_cam_buffer = np.frombuffer(mc, dtype=np.uint8).reshape(args.height, args.width, 4)
-                room_cam = Image.fromarray(r_cam_buffer, "RGBA").convert("RGB")
-                w_cam_buffer = np.frombuffer(wc, dtype=np.uint8).reshape(args.height, args.width, 4)
-                wrist_cam = Image.fromarray(w_cam_buffer, "RGBA").convert("RGB")
-                joint_pos = jp
 
-                actions = pi0_policy.infer(
-                    room_cam=np.array(room_cam),
-                    wrist_cam=np.array(wrist_cam),
-                    joint_pos=np.array(joint_pos[:7]),
-                )
-                actions = np.array(actions).astype(np.float32)
+    pi0_policy = PI0PolicyRunner(ckpt_path=args.ckpt_path, repo_id=args.repo_id)
 
-                # First action only
-                for action in actions:
-                    target_jp = [i for i in joint_pos]
-                    for i in range(len(action)):
-                        target_jp[i] = target_jp[i] + action[i]
-
-                    i = FrankaCtrlInput()
-                    i.joint_positions = target_jp
-                    writer.write(i)
-                    break
-
-    async def run_simulation():
-        await asyncio.gather(
-            read_room(),
-            read_wrist(),
-            read_pos(),
-        )
-
-    rti.asyncio.run(run_simulation())
+    r_cam_reader.start()
+    w_cam_reader.start()
+    f_pos_reader.start()
 
 
 if __name__ == "__main__":
