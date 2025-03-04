2c285ed9bac2c2e895ac6261de0abc2d3585b25f
Masoud Moghani <mmoghani@nvidia.com>
2025-03-04 15:00:21 -0800

    Add ORBIT-Surgical

---
+++ workflows/robotic_surgery/scripts/simulation/orbitsurgical.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024, The ORBIT-Surgical Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+${IsaacLab_PATH}/isaaclab.sh -p -m pip install -e exts/orbit.surgical.ext
+${IsaacLab_PATH}/isaaclab.sh -p -m pip install -e exts/orbit.surgical.assets
+${IsaacLab_PATH}/isaaclab.sh -p -m pip install -e exts/orbit.surgical.tasks
