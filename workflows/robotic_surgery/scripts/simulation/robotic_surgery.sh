d4e5a7cdcc7d58097d1bdc8c547570a006668c40
Masoud Moghani <moghani@cs.toronto.edu>
2025-03-13 09:34:55 -0700

    Add ORBIT-Surgical (#23)
    Fixes
    [#22](https://github.com/isaac-for-healthcare/i4h-workflows/issues/22)
    
    ## Description
    
    This MR adds the ORBIT-Surgical project to the workflows -
    robotic_surgery.

---
+++ workflows/robotic_surgery/scripts/simulation/robotic_surgery.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2024-2025, The ORBIT-Surgical Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+python -m pip install -e workflows/robotic_surgery/scripts/simulation/exts/robotic.surgery.assets
+python -m pip install -e workflows/robotic_surgery/scripts/simulation/exts/robotic.surgery.tasks
---
+++ workflows/robotic_surgery/scripts/simulation/utils/assets.py
@@ -0,0 +1,40 @@
+import os
+from dataclasses import dataclass
+
+from i4h_asset_helper import get_i4h_local_asset_path
+
+
+@dataclass
+class Enums:
+    """Enums for the assets in the robotic surgery workflow."""
+
+    dVRK_ECM = "Robots/dVRK/ECM/ecm.usd"
+    dVRK_PSM = "Robots/dVRK/PSM/psm_col.usd"
+    STAR = "Robots/STAR/star.usd"
+    Board = "Props/Board/board.usd"
+    Block = "Props/PegBlock/block.usd"
+    Needle = "Props/SutureNeedle/needle_sdf.usd"
+    SuturePad = "Props/SuturePad/suture_pad.usd"
+    Table = "Props/Table/table.usd"
+
+
+class Assets(Enums):
+    """Assets for the robotic surgery workflow customized for the user download directory."""
+
+    def __init__(self, download_dir: str | None = None):
+        """
+        Initialize the assets
+
+        Args:
+            download_dir: The directory to download the assets to
+        """
+        if download_dir is None:
+            download_dir = get_i4h_local_asset_path()
+        # Update all the paths
+        for attr in dir(self):
+            if not attr.startswith("_"):
+                setattr(self, attr, os.path.join(download_dir, getattr(self, attr)))
+
+
+# singleton object for the assets
+robotic_surgery_assets = Assets()
