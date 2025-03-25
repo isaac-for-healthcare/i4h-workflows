04bcb55b55282b0af867b97286f1851b90ccd6a3
Mingxin Zheng <mingxinz@nvidia.com>
2025-03-25 01:54:21 +0000

    fix
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- workflows/robotic_ultrasound/tests/test_simulation/test_assets.py
+++ workflows/robotic_ultrasound/tests/test_simulation/test_assets.py
@@ -1,8 +1,9 @@
 import sys
 import unittest
 
-from simulation.utils.assets import robotic_ultrasound_assets as robot_us_assets
 from i4h_asset_helper import get_i4h_local_asset_path
+from simulation.utils.assets import robotic_ultrasound_assets as robot_us_assets
+
 
 class TestAssets(unittest.TestCase):
     def setUp(self):
@@ -11,9 +12,9 @@ class TestAssets(unittest.TestCase):
     def test_import_failure(self):
         # delete the assets module
         from simulation.configs.basic import config
+
         self.assertEqual(config.main_usd_path, get_i4h_local_asset_path() + "/Test/basic.usda")
         sys.modules.pop("simulation.configs.basic")  # un-import the config
-
 
     def test_download_dir_setter(self):
         # Change the download directory
@@ -21,15 +22,14 @@ class TestAssets(unittest.TestCase):
         robot_us_assets.download_dir = new_dir
         # import Config to test that change was reflected
         from simulation.configs.basic import config
+
         self.assertEqual(config.main_usd_path, "/path/to/new/Test/basic.usda")
         sys.modules.pop("simulation.configs.basic")  # un-import the config
-
 
     def tearDown(self):
         for module_name in list(sys.modules.keys()):
             if module_name.startswith("simulation.utils"):
                 sys.modules.pop(module_name)
-
 
 
 if __name__ == "__main__":
