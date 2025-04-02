15db9c366d0dbdcdd834c9572c886dfd8de24584
Mingxin Zheng <mingxinz@nvidia.com>
2025-04-02 03:12:56 +0000

    Upgrade robotic surgery workflow to use IsaacSim 4.5 and IsaacLab 2.0.2
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- tools/env_setup_robot_surgery.sh
+++ tools/env_setup_robot_surgery.sh
@@ -43,20 +43,18 @@ fi
 
 
 # ---- Clone IsaacLab ----
-echo "Installing IsaacLab..."
+echo "Cloning IsaacLab..."
 # CLONING REPOSITORIES INTO PROJECT_ROOT/third_party
 echo "Cloning repositories into $PROJECT_ROOT/third_party..."
 mkdir $PROJECT_ROOT/third_party
-git clone -b v1.2.0 git@github.com:isaac-sim/IsaacLab.git $PROJECT_ROOT/third_party/IsaacLab
+git clone -b v2.0.2 git@github.com:isaac-sim/IsaacLab.git $PROJECT_ROOT/third_party/IsaacLab
 pushd $PROJECT_ROOT/third_party/IsaacLab
-sed -i "s/rsl-rl/rsl-rl-lib/g" source/extensions/omni.isaac.lab_tasks/setup.py
 
 
 # ---- Install IsaacSim and necessary dependencies ----
 echo "Installing IsaacSim..."
-pip install isaacsim==4.1.0.0 isaacsim-extscache-physics==4.1.0.0 \
-    isaacsim-extscache-kit==4.1.0.0 isaacsim-extscache-kit-sdk==4.1.0.0 \
-    git+ssh://git@github.com/isaac-for-healthcare/i4h-asset-catalog.git@v0.1.0ea \
+pip install 'isaacsim[all,extscache]==4.5.0' \
+    git+ssh://git@github.com/isaac-for-healthcare/i4h-asset-catalog.git@mz/isaacsim45 \
     --extra-index-url https://pypi.nvidia.com
 
 
