cf82d8b3d980b4883cddf35ebe2902dd106eeae8
Mingxin Zheng <mingxinz@nvidia.com>
2025-04-11 00:40:43 +0000

    fix integration test
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- workflows/robotic_ultrasound/scripts/simulation/imitation_learning/__init__.py
+++ workflows/robotic_ultrasound/scripts/simulation/imitation_learning/__init__.py
@@ -12,28 +12,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import unittest
-
-from helpers import run_with_monitoring
-from parameterized import parameterized
-
-SM_CASES = [
-    (
-        "python -u -m simulation.imitation_learning.pi0_policy.eval --enable_camera --headless",
-        120,
-        "Resetting the environment.",
-    ),
-]
-
-
-class TestPolicyEval(unittest.TestCase):
-    @parameterized.expand(SM_CASES)
-    def test_policy_eval(self, command, timeout, target_line):
-        # Run and monitor command
-        _, found_target = run_with_monitoring(command, timeout, target_line)
-        self.assertTrue(found_target)
-
-
-if __name__ == "__main__":
-    unittest.main()
