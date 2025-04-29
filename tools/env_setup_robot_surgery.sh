92336c2066708b6ab8f5ba33927722cefd44ec61
Mingxin Zheng <mingxinz@nvidia.com>
2025-04-29 08:14:36 +0000

    change to main
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- tools/env_setup_robot_surgery.sh
+++ tools/env_setup_robot_surgery.sh
@@ -54,7 +54,7 @@ pushd $PROJECT_ROOT/third_party/IsaacLab
 # ---- Install IsaacSim and necessary dependencies ----
 echo "Installing IsaacSim..."
 pip install 'isaacsim[all,extscache]==4.5.0' \
-    git+ssh://git@github.com/isaac-for-healthcare/i4h-asset-catalog.git@mz/fix_env \
+    git+ssh://git@github.com/isaac-for-healthcare/i4h-asset-catalog.git@main \
     --extra-index-url https://pypi.nvidia.com
 
 
