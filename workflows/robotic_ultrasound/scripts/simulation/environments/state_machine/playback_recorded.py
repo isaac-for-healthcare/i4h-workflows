080dbe187e1d2f52bffd66bc38b359787de8064f
YunLiu <55491388+KumoLiu@users.noreply.github.com>
2025-05-07 18:36:27 +0800

    fix format
    Signed-off-by: YunLiu <55491388+KumoLiu@users.noreply.github.com>

--- workflows/robotic_ultrasound/scripts/simulation/environments/state_machine/playback_recorded.py
+++ workflows/robotic_ultrasound/scripts/simulation/environments/state_machine/playback_recorded.py
@@ -1,21 +1,25 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Copyright (c) 2022-2024, The Isaac Lab Project Developers.
 # All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-"""
-This script plays back recorded demonstrations from HDF5 format.
-
-.. code-block:: bash
-
-    # Usage for HDF5 format
-    ## Absolute actions
-    python scripts/state_machine/playback_recorded.py --data_path /media/m3/repos/robotic_ultrasound_internal/data/hdf5/2025-01-24-Isaac-Teleop-Torso-FrankaUsRs-IK-RL-Rel-v0/valid_copy --task Isaac-Reach-Torso-FrankaUsRs-IK-RL-Abs-v0 --enable_camera
-    ## Relative actions
-    python scripts/state_machine/playback_recorded.py --data_path /media/m3/repos/robotic_ultrasound_internal/data/hdf5/2025-01-24-Isaac-Teleop-Torso-FrankaUsRs-IK-RL-Rel-v0/valid_copy --task Isaac-Teleop-Torso-FrankaUsRs-IK-RL-Rel-v0 --enable_camera --use_rel
-"""
-
 import argparse
+
 from isaaclab.app import AppLauncher
 
 # add argparse arguments
@@ -25,19 +29,8 @@ parser.add_argument(
 )
 parser.add_argument("--num_envs", type=int, default=1, help="Number of environments to spawn.")
 parser.add_argument("--task", type=str, default="Isaac-Teleop-Torso-FrankaUsRs-IK-RL-Rel-v0", help="Name of the task.")
-parser.add_argument(
-    "--num_episodes", 
-    type=int, 
-    default=0, 
-    help="Number of episodes to collect. If 0, no data collection is performed."
-)
 parser.add_argument("--use_rel", action="store_true", default=False, help="Use relative actions.")
-parser.add_argument(
-    "--data_path", 
-    type=str, 
-    required=True,
-    help="Path to either .hdf5 file containing recorded data."
-)
+parser.add_argument("--data_path", type=str, required=True, help="Path to either .hdf5 file containing recorded data.")
 
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
@@ -49,73 +42,76 @@ app_launcher = AppLauncher(args_cli)
 simulation_app = app_launcher.app
 
 """Rest everything follows."""
-import gymnasium as gym
-import torch
 import os
+
+import gymnasium as gym
 import h5py
-
+import torch
 from isaaclab_tasks.utils.parse_cfg import parse_env_cfg
-
 # Import extensions to set up environment tasks
 from robotic_us_ext import tasks  # noqa: F401
-
-from simulation.environments.state_machine.utils import get_robot_obs, compute_relative_action
+from simulation.environments.state_machine.utils import compute_relative_action, get_robot_obs
 
 
 def get_hdf5_episode_data(root, action_key: str):
     """Get episode data from HDF5 format."""
-    base_path = f'data/demo_0'
+    base_path = "data/demo_0"
     try:
-        actions = root[f'{base_path}/{action_key}'][()]
+        actions = root[f"{base_path}/{action_key}"][()]
     except KeyError as e:
         print(f"Error loading data using key: {base_path}/{action_key}")
         print(f"Available keys: {list(root[base_path].keys())}")
         raise e
     return actions
 
+
 def get_observation_episode_data(data_path: str, episode_idx: int, key: str):
     """Get episode data from HDF5 format for a given key."""
-    root = h5py.File(os.path.join(data_path, f"data_{episode_idx}.hdf5"), 'r')
+    root = h5py.File(os.path.join(data_path, f"data_{episode_idx}.hdf5"), "r")
     data = get_hdf5_episode_data(root, key)
     return data
+
 
 def get_episode_data(data_path: str, episode_idx: int):
     """Get episode data from HDF5 format."""
     # Load initial episode data
     try:
         action_key = "action" if args_cli.use_rel else "abs_action"
-        root = h5py.File(os.path.join(data_path, f"data_{episode_idx}.hdf5"), 'r')
+        root = h5py.File(os.path.join(data_path, f"data_{episode_idx}.hdf5"), "r")
         # total_episodes are the number of files starting with data__ in the directory
-        total_episodes = len([k for k in os.listdir(data_path) if k.startswith('data_')])
+        total_episodes = len([k for k in os.listdir(data_path) if k.startswith("data_")])
         actions = get_hdf5_episode_data(root, action_key)
         return actions, total_episodes
     except Exception as e:
         print(f"Error loading data: {str(e)}")
         return None, None
 
+
 def reset_organ_to_position(env, object_position):
     """Reset the organ position."""
-    organs = env.unwrapped.scene._rigid_objects['organs']
+    organs = env.unwrapped.scene._rigid_objects["organs"]
     root_state = organs.data.default_root_state.clone()
-    root_state[:, :7] = torch.tensor(object_position[0,:7], device='cuda:0')
-    # write to the sim 
+    root_state[:, :7] = torch.tensor(object_position[0, :7], device="cuda:0")
+    # write to the sim
     organs.write_root_state_to_sim(root_state)
+
 
 def reset_robot_to_position(env, robot_initial_joint_state):
     """Reset the robot to the initial joint state."""
     robot = env.unwrapped.scene["robot"]
-    joint_pos = torch.tensor(robot_initial_joint_state[0], device='cuda:0').unsqueeze(0)
+    joint_pos = torch.tensor(robot_initial_joint_state[0], device="cuda:0").unsqueeze(0)
     joint_vel = torch.zeros_like(joint_pos)
     # set joint positions
     robot.write_joint_state_to_sim(joint_pos, joint_vel)
     robot.reset()
+
 
 def main():
     """Main function."""
     # Check for HDF5 format by inspecting files in the directory
     if os.path.isdir(args_cli.data_path):
         files = os.listdir(args_cli.data_path)
-        if not any(file.endswith('.hdf5') for file in files):
+        if not any(file.endswith(".hdf5") for file in files):
             raise AssertionError("Unsupported file format. Please use a directory containing .hdf5 files.")
     else:
         raise AssertionError("Unsupported file format. Please use a directory containing .hdf5 files.")
@@ -124,11 +120,10 @@ def main():
     env_cfg = parse_env_cfg(
         args_cli.task, device=args_cli.device, num_envs=args_cli.num_envs, use_fabric=not args_cli.disable_fabric
     )
-    
+
     # create environment
     env = gym.make(args_cli.task, cfg=env_cfg)
-    
-    
+
     # reset environment at start
     obs = env.reset()
 
@@ -138,13 +133,12 @@ def main():
     episode_idx = 0
     action_idx = 0
     reset_steps = 50
-    
 
     # simulate physics
     while simulation_app.is_running():
-        torso_obs_key = 'observations/torso_obs'
+        torso_obs_key = "observations/torso_obs"
         actions, total_episodes = get_episode_data(args_cli.data_path, episode_idx)
-        
+
         # Check if we've reached the end of available episodes
         if episode_idx >= total_episodes - 1 or actions is None:
             print(f"Reached the end of available episodes ({episode_idx + 1}/{total_episodes})")
@@ -154,14 +148,16 @@ def main():
         reset_organ_to_position(env, object_position)
 
         # reset robot to initial joint state
-        joint_state_key = 'abs_joint_pos' 
+        joint_state_key = "abs_joint_pos"
         robot_initial_joint_state = get_observation_episode_data(args_cli.data_path, episode_idx, joint_state_key)
         reset_robot_to_position(env, robot_initial_joint_state)
-        
-        reset_tensor = torch.tensor(get_observation_episode_data(args_cli.data_path, episode_idx, 'observations/robot_obs')[0], device='cuda:0')
+
+        reset_tensor = torch.tensor(
+            get_observation_episode_data(args_cli.data_path, episode_idx, "observations/robot_obs")[0], device="cuda:0"
+        )
         with torch.inference_mode():
             for episode_idx in range(total_episodes):
-                print(f'\nepisode_idx: {episode_idx}')
+                print(f"\nepisode_idx: {episode_idx}")
                 # reset the ee_frame to the initial position
                 for _ in range(reset_steps):
                     robot_obs = get_robot_obs(env)
@@ -172,13 +168,13 @@ def main():
                     # Get next action from recorded data
                     current_action = torch.tensor(action, device=args_cli.device)
                     current_action = current_action.unsqueeze(0)
-                    
+
                     print(f"Step {action_step}/{len(actions)} | Episode {episode_idx + 1}/{total_episodes}")
                     print(f"Action: {current_action}")
 
                     # Step environment with recorded action
                     obs, rew, terminated, truncated, info_ = env.step(current_action)
-                    
+
                     # Update counters
                     action_idx += 1
 
@@ -187,18 +183,21 @@ def main():
                 if episode_idx + 1 >= total_episodes:
                     print(f"Completed all episodes ({total_episodes})")
                     break
-                    
+
                 actions, _ = get_episode_data(args_cli.data_path, episode_idx + 1)
                 object_position = get_observation_episode_data(args_cli.data_path, episode_idx + 1, torso_obs_key)
                 reset_organ_to_position(env, object_position)
-                robot_initial_joint_state = get_observation_episode_data(args_cli.data_path, episode_idx + 1, joint_state_key)
+                robot_initial_joint_state = get_observation_episode_data(
+                    args_cli.data_path, episode_idx + 1, joint_state_key
+                )
                 reset_robot_to_position(env, robot_initial_joint_state)
 
     # close the environment and data file
     env.close()
 
+
 if __name__ == "__main__":
     # run the main function
     main()
     # close sim app
-    simulation_app.close() 
+    simulation_app.close()
