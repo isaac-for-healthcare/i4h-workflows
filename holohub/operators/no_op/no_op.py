6f1e6ffd6dd8aeead9a9063f812e72cc259e2555
Wendell Hom <whom@nvidia.com>
2025-06-05 20:09:19 -0700

    Refactor realsense operator

---
+++ holohub/operators/no_op/no_op.py
@@ -0,0 +1,32 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from holoscan.core import Operator
+from holoscan.core._core import OperatorSpec
+
+
+class NoOp(Operator):
+    """A sink operator that takes input and discards them."""
+    def __init__(self, fragment, input_ports = ["input"], *args, **kwargs):
+        self.input_ports = input_ports
+        super().__init__(fragment, *args, **kwargs)
+
+    def setup(self, spec: OperatorSpec):
+        for port in self.input_ports:
+            spec.input(port)
+
+    def compute(self, op_input, op_output, context):
+        for port in self.input_ports:
+            op_input.receive(port)
