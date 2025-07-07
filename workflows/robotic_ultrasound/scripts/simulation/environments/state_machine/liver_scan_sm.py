6348b60c66d17acbbe27c72a65ab8949cfed8517
Mingxin Zheng <mingxinz@nvidia.com>
2025-07-07 13:09:00 +0000

    add resest script and fix import
    Signed-off-by: Mingxin Zheng <mingxinz@nvidia.com>

--- workflows/robotic_ultrasound/scripts/simulation/environments/state_machine/liver_scan_sm.py
+++ workflows/robotic_ultrasound/scripts/simulation/environments/state_machine/liver_scan_sm.py
@@ -109,7 +109,7 @@ simulation_app = app_launcher.app
 import gymnasium as gym  # noqa: F401
 import torch  # noqa: F401
 from isaaclab_tasks.utils.parse_cfg import parse_env_cfg  # noqa: F401
-from meta_state_machine.ultrasound_state_machine import UltrasoundStateMachine  # noqa: F401
+from simulation.environments.state_machine.meta_state_machine.ultrasound_state_machine import UltrasoundStateMachine  # noqa: F401
 from robotic_us_ext import tasks  # noqa: F401
 from simulation.environments.state_machine.data_collection import DataCollectionManager  # noqa: F401
 from simulation.environments.state_machine.modules.force_module import ForceControlModule  # noqa: F401
