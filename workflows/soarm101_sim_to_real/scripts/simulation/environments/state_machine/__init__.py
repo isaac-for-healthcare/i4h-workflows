355fc94e146de3464e60a70f300ee010d037a6ee
Mingxue Gu <mingxueg@nvidia.com>
2025-07-25 22:04:18 +0800

    remove necessary lib

--- workflows/soarm101_sim_to_real/scripts/simulation/environments/state_machine/__init__.py
+++ workflows/soarm101_sim_to_real/scripts/simulation/environments/state_machine/__init__.py
@@ -17,6 +17,6 @@
 State machine package for robotic control.
 """
 
-from . import meta_state_machine, modules
+from . import modules
 
-__all__ = ["meta_state_machine", "modules"]
+__all__ = ["modules"]
